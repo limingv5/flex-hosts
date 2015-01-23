87ff70ea94385613d67dc89342b36e5edf629b8a
limingv5 <limingv5@gmail.com>
2015-01-23 19:06:30 +0800

    test

--- test/main.js
+++ test/main.js
@@ -2,3 +2,9 @@ var FlexHosts = require("../api");
 var param = require("../lib/param");
 
 var flexHosts = new FlexHosts(param);
+
+var timer = setTimeout(function() {
+  flexHosts.restore(function() {
+    clearTimeout(timer);
+  })
+}, 3000);
