abf822698c64bff26758ee3b012ee6b2eb06aed3
limingv5 <limingv5@gmail.com>
2015-05-14 23:45:06 +0800

    ip

--- api.js
+++ api.js
@@ -211,11 +211,12 @@ FlexHosts.prototype = {
 
       serverIP = serverIP || req.headers.host;
       var clientIP = req.connection.remoteAddress.replace(/.+\:/, '');
+      clientIP = (net.isIP(clientIP) && clientIP != "127.0.0.1") ? clientIP : serverIP;
       var Q = urlLib.parse(req.url).query;
       var queryCIP = Q ? querystring.parse(Q).client : null;
 
       if (["localhost", "127.0.0.1", clientIP].indexOf(serverIP) != -1) {
-        if (queryCIP && net.isIP(queryCIP)) {
+        if (queryCIP && net.isIP(queryCIP) && ["localhost", "127.0.0.1"].indexOf(queryCIP) == -1) {
           clientIP = queryCIP;
         }
         else {
